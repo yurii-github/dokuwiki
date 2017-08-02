93e646facb1ce2dfab8cbd0096234cf6eafba3b3
Yurii K <yurii-github@users.noreply.github.com>
2017-08-02 17:02:33 +0300

    cleaning

--- lib/tpl/harchi/main.php
+++ lib/tpl/harchi/main.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * DokuWiki Default Template 2012
- *
- * @link     http://dokuwiki.org/template
- * @author   Anika Henke <anika@selfthinker.org>
- * @author   Clarence Lee <clarencedglee@gmail.com>
- * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
- */
-
 if (!defined('DOKU_INC')) die(); /* must be run from within DokuWiki */
 header('X-UA-Compatible: IE=edge,chrome=1');
 
