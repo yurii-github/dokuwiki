80de1276911bf8ece833a9993c6bad6223a049ca
Yurii K <yurii-github@users.noreply.github.com>
2017-08-02 16:32:16 +0300

    comments

--- lib/tpl/harchi/main.php
+++ lib/tpl/harchi/main.php
@@ -124,6 +124,23 @@ $showSidebar = $hasSidebar && ($ACT=='show');
             </div>
         </div><!-- /wrapper -->
 
+        <?php if ($ACT=='show'): ?>
+        <div id="hypercomments_widget"></div>
+        <script type="text/javascript">
+            _hcwp = window._hcwp || [];
+            _hcwp.push({widget:"Stream", widget_id: 91700});
+            (function() {
+                if("HC_LOAD_INIT" in window)return;
+                HC_LOAD_INIT = true;
+                var lang = '<?= $conf['lang'];?>';
+                var hcc = document.createElement("script"); hcc.type = "text/javascript"; hcc.async = true;
+                hcc.src = ("https:" == document.location.protocol ? "https" : "http")+"://w.hypercomments.com/widget/hc/91700/"+lang+"/widget.js";
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(hcc, s.nextSibling);
+            })();
+        </script>
+        <a href="http://hypercomments.com" class="hc-link" title="comments widget">comments powered by HyperComments</a>
+        <?php endif; ?>
         <?php include('tpl_footer.php') ?>
     </div></div><!-- /site -->
 
